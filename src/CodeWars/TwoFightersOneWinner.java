6ea9093ddd1650594138be2f0cd8328b1d4c67af
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-06-29 00:15:56 +0400

    * Fixed

--- src/CodeWars/TwoFightersOneWinner.java
+++ src/CodeWars/TwoFightersOneWinner.java
@@ -3,12 +3,14 @@ package CodeWars;
 
 class Fighter {
     private String name;
-    private int health, damagePerAttack;
+    private int health, damagePerAttack,critRate;
 
-    Fighter(String name, int health, int damagePerAttack) {
+    Fighter(String name, int health, int damagePerAttack, int critRate) {
         this.name = name;
         this.health = health;
         this.damagePerAttack = damagePerAttack;
+        this.critRate = critRate;
+
     }
 
     static String declareWinner(
@@ -17,23 +19,32 @@ class Fighter {
             String firstAttacker) {
 
         int count;
+        double critChance = 0.85;
 
         if (firstAttacker.equals(fighter1.name)) {
             fighter2.health -= fighter1.damagePerAttack;
+            if (Math.random()> critChance)
+            {fighter2.health -= (fighter1.damagePerAttack*fighter1.critRate);}
             count = 2;
         } else {
             fighter1.health -= fighter2.damagePerAttack;
+            if (Math.random()> critChance)
+            {fighter1.health -= (fighter2.damagePerAttack*fighter2.critRate);}
             count = 1;
         }
 
         do {
             if (count % 2 != 0) {
                 fighter2.health -= fighter1.damagePerAttack;
+                if (Math.random()> critChance)
+                {fighter2.health -= (fighter1.damagePerAttack*fighter1.critRate);}
                 count++;
                 if (fighter2.health <= 0) break;
 
             } else {
                 fighter1.health -= fighter2.damagePerAttack;
+                if (Math.random()> critChance)
+                {fighter1.health -= (fighter2.damagePerAttack*fighter2.critRate);}
                 count++;
                 if (fighter1.health <= 0) break;
             }
@@ -60,12 +71,12 @@ class Fighter {
 public class TwoFightersOneWinner {
     public static void main(String[] args) {
 
-        System.out.println(Fighter.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
-        System.out.println(Fighter.declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"));
-        System.out.println(Fighter.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
-        System.out.println(Fighter.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"));
-        System.out.println(Fighter.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"));
-        System.out.println(Fighter.declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"));
+        System.out.println(Fighter.declareWinner(new Fighter("Lew", 10, 2,0), new Fighter("Harry", 5, 4,1), "Lew"));
+        System.out.println(Fighter.declareWinner(new Fighter("Lew", 10, 2,0), new Fighter("Harry", 5, 4,1), "Harry"));
+        System.out.println(Fighter.declareWinner(new Fighter("Harald", 20, 5,0), new Fighter("Harry", 5, 4,1), "Harry"));
+        System.out.println(Fighter.declareWinner(new Fighter("Harald", 20, 5,0), new Fighter("Harry", 5, 4,1), "Harald"));
+        System.out.println(Fighter.declareWinner(new Fighter("Jerry", 30, 3,2), new Fighter("Harald", 20, 5,0), "Jerry"));
+        System.out.println(Fighter.declareWinner(new Fighter("Jerry", 30, 3,2), new Fighter("Harald", 20, 5,0), "Harald"));
 
     }
 }
