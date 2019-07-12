b65256185223f09b850836645537f3851d8467e8
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-07-12 09:51:21 +0400

    Java8.6thEdition-10chapter, complete KtoD

---
+++ src/pack10ch/fileReaderPack/KtoD.java
@@ -0,0 +1,37 @@
+package pack10ch.fileReaderPack;
+
+import java.io.*;
+
+public class KtoD {
+    public static void main(String[] args) {
+        String str;
+
+        BufferedReader stringValue =
+                new BufferedReader(
+                        new InputStreamReader(System.in));
+
+        System.out.println("Input \"stop\" for complete");
+
+        try (FileWriter testFWtxt = new FileWriter(
+                myAddress() + "testFW.txt")) {
+            do {
+
+                System.out.println("next input:");
+                str = stringValue.readLine();
+
+                if (str.compareTo("stop") == 0) break;
+
+                str = str + "\r\n";
+                testFWtxt.write(str);
+
+            } while (str.compareTo("stop") != 0);
+        } catch (IOException exc) {
+            System.out.println("IO Exception: " + exc);
+        }
+    }
+
+    private static String myAddress() {
+        return "C:\\Users\\Admin\\IdeaProjects" +
+                "\\myfirstproj\\src\\pack10ch\\fileReaderPack\\";
+    }
+}
