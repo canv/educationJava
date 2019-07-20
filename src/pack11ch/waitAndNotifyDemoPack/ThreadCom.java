ac84c03257c4a926cdbcb16bf24580930ee89447
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-07-20 23:07:35 +0400

    Java8.6thEdition-11chapter, complete waitAndNotifyDemoPack.ThreadCom

---
+++ src/pack11ch/waitAndNotifyDemoPack/ThreadCom.java
@@ -0,0 +1,95 @@
+package pack11ch.waitAndNotifyDemoPack;
+
+import java.io.*;
+
+class TickTock {
+    private String state;
+
+    synchronized void tick(boolean running) {
+        if (!running) {
+            state = "TICKed";
+            notify();
+            return;
+        }
+
+        ThreadCom.consoleOut.print("Tick ");
+        state = "TICKed";
+        notify();
+        try {
+            while (!state.equals("TOCKed"))
+                wait();
+        } catch (InterruptedException exc) {
+            ThreadCom.consoleOut.println("Interrupted thread(TICKed)!");
+        }
+    }
+
+    synchronized void tock(boolean running) {
+        if (!running) {
+            state = "TOCKed";
+            notify();
+            return;
+        }
+
+        ThreadCom.consoleOut.println("Tock ");
+        state = "TOCKed";
+        notify();
+        try {
+            while (!state.equals("TICKed"))
+                wait();
+        } catch (InterruptedException exc) {
+            ThreadCom.consoleOut.println("Interrupted thread(TOCKed)!");
+        }
+    }
+}
+
+class MyThread implements Runnable {
+    private Thread thread;
+    private TickTock tickTockObj;
+
+    MyThread(String name, TickTock tickTockObj) {
+        thread = new Thread(this, name);
+        this.tickTockObj = tickTockObj;
+
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        switch (thread.getName()) {
+
+            case "Tick":
+                for (int i = 0; i < 5; i++) tickTockObj.tick(true);
+                tickTockObj.tick(false);
+                break;
+
+            case "Tock":
+                for (int i = 0; i < 5; i++) tickTockObj.tock(true);
+                tickTockObj.tock(false);
+                break;
+
+            default:
+                ThreadCom.consoleOut.println("Thread name reading error!");
+        }
+    }
+
+    Thread getThread() {
+        return thread;
+    }
+}
+
+class ThreadCom {
+    public static void main(String[] args) {
+        TickTock tt = new TickTock();
+        MyThread mt1 = new MyThread("Tick", tt);
+        MyThread mt2 = new MyThread("Tock", tt);
+
+        try {
+            mt1.getThread().join();
+            mt2.getThread().join();
+        } catch (InterruptedException exc) {
+            consoleOut.println("Main thread interrupted!");
+        }
+    }
+
+    static PrintWriter consoleOut = new PrintWriter(System.out, true);
+}
