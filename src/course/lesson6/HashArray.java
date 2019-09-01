e29d54a688bbe7840593ca51aad95787363aae63
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-09-01 23:58:56 +0400

    Java.ASClasses-lesson6, reFix(2) Lesson6

--- src/course/lesson6/HashArray.java
+++ src/course/lesson6/HashArray.java
@@ -4,19 +4,25 @@ import static packAlternative.AOutput.consoleOut;
 
 public class HashArray<K,V> {
     private static final int INITIAL_SIZE = 1_000_000;
-    private MyLinkedList[] hashArray = new MyLinkedList[INITIAL_SIZE];
+    private MyLinkedList[] hashArray = new MyLinkedList[INITIAL_SIZE+1];
 
     public void put(K key, V val) {
         int hash = entryAddress(key);
+       // consoleOut.println("~ key: " + key + " val: " + val + " hash: " + hash);
 
         if (hashArray[hash] == null) {
+          //  System.out.println("0");
+
             MyLinkedList<K, V> entry = new MyLinkedList<>();
             entry.add(key, val);
             hashArray[hash] = entry;
         } else {
-            if(key.equals(hashArray[hash].getKey()))
-                hashArray[hash].overwrite(key, val);
-            else hashArray[hash].add(key, val);
+           // consoleOut.println("put's first else");
+            if(hashArray[hash].coincidenceCorrection(key,val));
+            else {
+             //   System.out.println("\tkey - [" + key + "] hashArray[hash].getCurrentKey() [" + (hashArray[hash].getKey()) + "]");
+                hashArray[hash].add(key, val);
+            }
         }
     }
 
@@ -29,7 +35,12 @@ public class HashArray<K,V> {
         return null;
     }
 
+
     private int entryAddress(K key){
-        return key.hashCode() * 32 & INITIAL_SIZE;
+//        consoleOut.println("~*~ hash: " + (key.hashCode() & INITIAL_SIZE));
+//        consoleOut.println("~~ hash: " + (key.hashCode() * 32));
+//        consoleOut.println("~~ hash: " + (key.hashCode()));
+
+        return key.hashCode() & INITIAL_SIZE;
     }
 }
