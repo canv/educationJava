3687193023f97ffa05d347732b27a6d9a7177669
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-09-04 23:41:13 +0400

    Java.ASClasses-lesson9, fix City in Lesson9

---
+++ src/course/lesson9/CitySecrots.java
@@ -0,0 +1,70 @@
+package course.lesson9;
+
+abstract class CitySectors implements CitySet{
+    private int id;
+    private String name;
+    private int length;
+    private int width;
+    private Damage damage;
+
+    CitySectors(String name, int length, int width, Damage damage) {
+        this.id = hashCode() & Integer.MAX_VALUE;
+        this.name = name;
+        this.length = length;
+        this.width = width;
+        this.damage = damage;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Damage getDamage() {
+        return damage;
+    }
+    @Override
+    public int getLength() {
+        return length;
+    }
+    @Override
+    public int getWidth() {
+        return width;
+    }
+}
+
+class Park extends CitySectors {
+    Park(String name, int length, int width, Damage damage) {
+        super(name, length, width, damage);
+    }
+}
+class Street extends CitySectors {
+    Street(String name, int length, int width, Damage damage) {
+        super(name, length, width, damage);
+    }
+}
+class LivingDistrict extends CitySectors {
+    LivingDistrict(String name, int length, int width, Damage damage) {
+        super(name, length, width, damage);
+    }
+}
+
+class Damage{
+    private int length;
+    private int width;
+
+    Damage(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    int getWidth() {
+        return width;
+    }
+}
